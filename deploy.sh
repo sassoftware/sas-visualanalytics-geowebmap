377e0e220bd984b26d77677dcfcf0c5525196e05
Tony Graham <tony.graham@sas.com>
2021-04-05 17:12:42 -0400

    adding comments to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Note: Copies build from DIST_DIR to docs/ and
+# adds a few files.
+#
+# To verify deployment, update the VERSION_NOTE
+# before push.
+#
+# GitHub pages currently configured to publish
+# from the "docs" directory of the "deploy" 
+# branch.
+
 VERSION_NOTE="sas-visualanalytics-geowebmap v1.3.4"
 DIST_DIR=../sas-visualanalytics-geowebmap/dist/
 
@@ -8,4 +18,5 @@ mkdir docs
 cp -r ${DIST_DIR} docs/
 echo "User-agent: *\nDisallow: /\n" > docs/robots.txt
 echo "${VERSION_NOTE}\n" > docs/version.html
+echo "${VERSION_NOTE}\n" > docs/index.md
 echo "== Deployment complete =="
