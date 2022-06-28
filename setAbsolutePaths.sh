d07c8f3c4550dbf754f0df9a41048f0c0ed9eda5
Tony Graham <tony.graham@sas.com>
2022-06-28 16:25:26 -0400

    reset root

--- setAbsolutePaths.sh
+++ setAbsolutePaths.sh
@@ -3,7 +3,7 @@
 # 2 places: package.json and src/settings.json.
 #
 # The absolute URI for the deployment, without a trailing slash:
-ABSOLUTE_ROOT="https:\/\/sdkcontentstorage.z13.web.core.windows.net\/geowebmap\/1.5.0"
+ABSOLUTE_ROOT=""
 #
 sed -i '' "s/\"homepage\"\: \".\"/\"homepage\": \"${ABSOLUTE_ROOT}\"/" package.json
 sed -i '' "s/\"homepage\"\: \".\"/\"homepage\": \"${ABSOLUTE_ROOT}\"/" src/settings.json
